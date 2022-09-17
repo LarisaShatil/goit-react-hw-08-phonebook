7e5c972642274f2c319552566e7565d90c0c4d7a
Larisa Shatillo <larisa.shat@ukr.net>
2022-09-17 20:01:40 +0300

    Updates

---
+++ precache-manifest.a96dbc6fb6dc8ea8a8308c8decf33914.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aebe72248d809bd768c",
+    "url": "/goit-react-hw-08-phonebook/static/js/0.5aebe722.chunk.js"
+  },
+  {
+    "revision": "1063b7b4ff8967cab9a3",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.416b61a8.chunk.css"
+  },
+  {
+    "revision": "1063b7b4ff8967cab9a3",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.1063b7b4.chunk.js"
+  },
+  {
+    "revision": "6103e1493a3fa15f02e0",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.6103e149.chunk.js"
+  },
+  {
+    "revision": "b6ead70cdc81fbc9fd1c",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.b6ead70c.chunk.js"
+  },
+  {
+    "revision": "86a1c794d066972f94b5",
+    "url": "/goit-react-hw-08-phonebook/static/js/4.86a1c794.chunk.js"
+  },
+  {
+    "revision": "86b4ec36ce1158737960",
+    "url": "/goit-react-hw-08-phonebook/static/css/5.98264366.chunk.css"
+  },
+  {
+    "revision": "86b4ec36ce1158737960",
+    "url": "/goit-react-hw-08-phonebook/static/js/5.86b4ec36.chunk.js"
+  },
+  {
+    "revision": "b511ea8422f41306d21d",
+    "url": "/goit-react-hw-08-phonebook/static/js/6.b511ea84.chunk.js"
+  },
+  {
+    "revision": "509ab20bf16099f7f2ff",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime~main.509ab20b.js"
+  },
+  {
+    "revision": "b593b9a8ffd426ca8aae29f97417ac68",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  }
+];
